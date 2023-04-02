7a197c8dd948da784eb9129071e09633a1831ef4
Dominik <domi500domi@gmail.com>
2023-04-02 20:23:19 +0200

    cc

---
+++ C#_Projects/otherProject/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj liczbe limit 100000");
+            string input = Console.ReadLine();
+            int liczba;
+            int reszta = 0;
+            while (true)
+            {
+                if (int.TryParse(input, out liczba))
+                {
+                    if (liczba > 100000)
+                    {
+                        Console.WriteLine("podales za duza liczbe");
+                        break;
+                    }
+                    for (int i = 1; i <= liczba; i = i * 2)
+                    {
+                        Console.WriteLine(i);
+                        if (i == liczba)
+                        {
+                            Console.WriteLine("Program sie zakonczyl");
+                        }
+                        reszta = liczba - i;
+                    }
+                    
+                    Console.WriteLine("liczby wypisane, dopisz kolejne reszta: ");
+                    Console.WriteLine(reszta);
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("podales ciag znakow!");
+                    break;
+                }
+            }
+           
+            
+            
+            
+        }
+    }
+}
