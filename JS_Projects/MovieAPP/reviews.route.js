b32902410ab11fef3b8fb6bf01ef275b42237494
Dominik <domi500domi@gmail.com>
2023-04-30 16:21:34 +0200

    wagr

--- JS_Projects/MovieAPP/reviews.route.js
+++ JS_Projects/MovieAPP/reviews.route.js
@@ -1,7 +1,9 @@
 import express from "express"
-
+import ReviewsCtrl from "./reviews.controller.js"
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("hello world"))
+router.route("/movie/:id").get(ReviewsCtrl.apiGetReviews)
+router.route("/new").post(ReviewsCtrl.apiPostReview)
+router.route(":id").get(ReviewsCtrl.apiGetReview).put(ReviewsCtrl.apiUpdateReview).delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
